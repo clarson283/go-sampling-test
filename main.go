aaee68830521025f887bdead04dc0276887e2acc
clarson283 <callie.larson@thecontrolgroup.com>
2020-12-05 14:25:14 -0800

    adding slice and ten random of slice

--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 func main() {
-	fmt.Println("hello sampling project.")
+	s := make([]int, 50)
+
+	for i := range s {
+		s[i] = rand.Intn(100)
+	}
+
+	n := make([]int, 10)
+	x := &s
+
+	for j := range n {
+		n[j] = s[rand.Intn(len(s))]
+	}
+
+	fmt.Println(*x)
+	fmt.Println(n)
 }
